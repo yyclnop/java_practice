250dda107b41576b2f0a51a1490badeaaf7c2761
yyclnop <yyclnop@hotmail.com>
2015-04-22 19:21:05 +0800

    modify

--- SendAttachmentInEmail/src/main/java/com/company/SendAttachmentInEmail.java
+++ SendAttachmentInEmail/src/main/java/com/company/SendAttachmentInEmail.java
@@ -4,9 +4,14 @@
 
 package com.company;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 
@@ -31,10 +36,10 @@ class myAuthenticator extends Authenticator {
 public class SendAttachmentInEmail {
     public static void main(String[] args) {
         // Recipient's email ID needs to be mentioned.
-        String to = "test2@163.com    ";
+        String to = "wowbtn2@163.com";
 
         // Sender's email ID needs to be mentioned
-        String from = "test@163.com";
+        String from = "wowbtn@163.com";
 
         // Assuming you are sending email from localhost
         String host = "smtp.163.com";
@@ -52,7 +57,7 @@ public class SendAttachmentInEmail {
         //方法1
         Session session1 = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("test@163.com", "123456");
+                return new PasswordAuthentication("wowbtn@163.com", "123456");
             }
         });
         //方法2
@@ -63,64 +68,6 @@ public class SendAttachmentInEmail {
         try {
             // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session1);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-
-            // Now set the actual message
-            message.setText("This is actual message");
-
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
-    }
-}
-
-/*
-*
-* import java.util.*;
-        import javax.mail.*;
-        import javax.mail.internet.*;
-        import javax.activation.*;
-
-* */
-
- class SendFileEmail1
-{
-    public static void main(String [] args)
-    {
-
-        // Recipient's email ID needs to be mentioned.
-        String to = "abcd@gmail.com";
-
-        // Sender's email ID needs to be mentioned
-        String from = "web@gmail.com";
-
-        // Assuming you are sending email from localhost
-        String host = "localhost";
-
-        // Get system properties
-        Properties properties = System.getProperties();
-
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
-
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-        try{
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
 
             // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
@@ -146,7 +93,7 @@ public class SendAttachmentInEmail {
 
             // Part two is attachment
             messageBodyPart = new MimeBodyPart();
-            String filename = "file.txt";
+            String filename = "R:\\Archive.7z";
             DataSource source = new FileDataSource(filename);
             messageBodyPart.setDataHandler(new DataHandler(source));
             messageBodyPart.setFileName(filename);
@@ -158,7 +105,7 @@ public class SendAttachmentInEmail {
             // Send message
             Transport.send(message);
             System.out.println("Sent message successfully....");
-        }catch (MessagingException mex) {
+        } catch (MessagingException mex) {
             mex.printStackTrace();
         }
     }
